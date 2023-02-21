233af845c13877794733aa60054d44ad26e23e59
Shraddha <jadhavshradddha5@gmail.com>
2023-02-21 09:37:17 +0530

    creatinf a stack

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import com.bridgelab.stack.StackProgram;
+
 public class Main {
     public static void main(String[] args) {
 
         //System.out.println("Hello world!");
+
+        StackProgram stack= new StackProgram();
+        // insert Stack value
+
+        stack.push(70);
+
+        stack.push(30);
+
+        stack.push(56);
+
+        // print Stack elements
+        stack.display();
+
+        // print Top element of Stack
+        System.out.printf("\nTop element is %d\n",
+                stack.peek());
     }
 }
